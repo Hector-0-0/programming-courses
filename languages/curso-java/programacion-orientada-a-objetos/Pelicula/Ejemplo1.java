4b9702e8dc198016f88342e9301b8e8ef40b29f2
Hector <floressanchezhectordavid@gmail.com>
2026-03-22 22:37:30 -0500

    22/03/26

--- languages/curso-java/programacion-orientada-a-objetos/Pelicula/Ejemplo1.java
+++ languages/curso-java/programacion-orientada-a-objetos/Pelicula/Ejemplo1.java
@@ -12,3 +12,15 @@ class Ejemplo {
 Ejemplo.metodoEstatico();  // ✓ Funciona sin new
 Ejemplo e = new Ejemplo();
 e.metodoNormal();           // ✓ Necesita instancia
+
+//esto es cuando se habla de un meotodo, pero si hablamos de una clase?
+class ClaseEstatic {
+    static class ClaseAnidada {
+        void mostrar() {
+            System.out.println("Clase anidada estática");
+        }
+    }
+}
+// Uso:
+ClaseEstatic.ClaseAnidada obj = new ClaseEstatic.ClaseAnidada();
+obj.mostrar();  // ✓ Acceso a clase anidada estática sin instancia de ClaseEstatic
---
+++ languages/curso-java/programacion-orientada-a-objetos/clases/Clases/Clases.java
@@ -0,0 +1,23 @@
+package clases.Clases;
+
+public class Clases {
+    
+    // Atributos
+    String Atributo1;
+    String Atributo2;
+    String Atributo3;
+
+    // Constructor
+    public Clases(){
+
+    }
+
+    // Métodos
+    public void Metodo1(){
+        System.out.println("Este es el método 1");
+    }
+
+    public void Metodo2(){
+        System.out.println("Este es el método 2");
+    }
+}
